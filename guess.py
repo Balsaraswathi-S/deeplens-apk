1fdf36db2ce07305e0dce5bfc9d62798271b9733
Balsaraswathi <balasuraj2829@gmail.com>
2020-03-05 11:21:44 +0530

    Fixing Comments

--- guess.py
+++ guess.py
@@ -4,10 +4,10 @@ import requests
 import hashlib
 import os
 
-bucket_name = os.environ['doorman-apk']
+bucket_name = os.environ['lensapk']
 slack_token = os.environ['xoxp-929407804161-941770225558-931346518001-c7e1d9ac7da10763c84094109931a528']
 slack_channel_id = os.environ['CTPNN7N3G']
-rekognition_collection_id = os.environ['deeplensapk']
+rekognition_collection_id = os.environ['deep-apk']
 
 
 def guess(event, context):
--- train.py
+++ train.py
@@ -8,10 +8,10 @@ try:
 except ImportError:
      from urlparse import urlparse
 
-bucket_name = os.environ['doorman-apk'] ##name of the bucket (S3)
+bucket_name = os.environ['lensapk'] ##name of the bucket (S3)
 slack_token = os.environ['xoxp-929407804161-941770225558-931346518001-c7e1d9ac7da10763c84094109931a528']
 slack_channel_id = os.environ['CTPNN7N3G']
-rekognition_collection_id = os.environ['deeplensapk']##name of the rekognition collection id
+rekognition_collection_id = os.environ['deep-apk']##name of the rekognition collection id
 
 def train(event, context):
     # print(event['body'])
